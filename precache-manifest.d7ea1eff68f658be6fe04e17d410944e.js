ef5acaa4a526ed960c6b21c2d024310570e29ba7
abdullah <hbkamk@gmail.com>
2020-07-17 14:14:26 +0600

    Updates

---
+++ precache-manifest.d7ea1eff68f658be6fe04e17d410944e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4242ed9e5a9b8b19243a2b8e2dba4912",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/index.html"
+  },
+  {
+    "revision": "3185a7dd2b5472ca973c",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/static/css/main.eb4503ed.chunk.css"
+  },
+  {
+    "revision": "1ba3a78e373589a03a61",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/static/js/2.62210710.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/static/js/2.62210710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3185a7dd2b5472ca973c",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/static/js/main.f990bccb.chunk.js"
+  },
+  {
+    "revision": "899c289c77a0c86ba96d",
+    "url": "//AbdullahMohammadKhan.github.io/quiz-app-1/static/js/runtime-main.1ac20695.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//AbdullahMohammadKhan.github.io/quiz-app-1/precache-manifest.d7ea1eff68f658be6fe04e17d410944e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//AbdullahMohammadKhan.github.io/quiz-app-1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
